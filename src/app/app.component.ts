a95f89d9b0dbaa906dc32e2db4b3bed9c23a3364
Ivan Kupchyk <ivan.kupchic@gmail.com>
2019-05-31 23:43:25 +0300

    minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -296,13 +296,13 @@ export class AppComponent {
 
     const firstScreenFirstHalfLineSize = 424 + (((height * 0.55) - (766 * 0.55)) * 0.755);
     this.firstScreenFirstHalfStyle = this.transform(
-      `background: linear-gradient(41deg, #6666007e ${firstScreenFirstHalfLineSize}px, #ff000001
+      `background: linear-gradient(41deg, #666600ce ${firstScreenFirstHalfLineSize}px, #ff000001
       ${firstScreenFirstHalfLineSize + 1}px, #ff000001 80%, #ff000001 80%)`
     );
     const firstScreenSecondHalfLineSize = 684 + (((height * 0.45) - (766 * 0.45)) * 1.675);
     this.firstScreenSecondHalfStyle = this.transform(
-      `background:  linear-gradient(41deg, #ffcc0099 ${firstScreenSecondHalfLineSize}px, #33333376
-       ${firstScreenSecondHalfLineSize + 1}px, #33333376 1%, #33333376 1%)` // ffcc00d4
+      `background:  linear-gradient(41deg, #ffcc00b9 ${firstScreenSecondHalfLineSize}px, #333333a6
+       ${firstScreenSecondHalfLineSize + 1}px, #333333a6 1%, #333333a6 1%)` // ffcc00d4
     );
 
     // second page
