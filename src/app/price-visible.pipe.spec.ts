73c46045f88bde33630796d3eb4139370e547609
Ivan Kupchyk <ivan.kupchic@gmail.com>
2019-07-20 12:17:17 +0300

    Deleted tests

--- src/app/price-visible.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PriceVisiblePipe } from './price-visible.pipe';
-
-describe('PriceVisiblePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PriceVisiblePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
